654e1d9eee73690d487a2b3a9fda02783403a1a4
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-04-28 15:31:50 +0200

    modify routes to reflect models scoped views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :sellers, path: 'sellers'
+  devise_for :sellers, path: 'sellers', controllers: {sessions: 'sellers/sessions',
+     confirmation: 'sellers/confirmations',
+     passwords: 'sellers/passwords',
+     registerations: 'sellers/registrations',
+  }
+
   get 'welcome/index'
-  devise_for :users, path: 'users'
+  devise_for :users, path: 'users', controllers: {sessions: 'users/sessions',
+    confirmation: 'users/confirmations',
+    passwords: 'users/passwords',
+    registerations: 'users/registrations',
+ }
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
