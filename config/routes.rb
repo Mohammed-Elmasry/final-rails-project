954c44d82c869d7e319e375ca8d71e5b89f2935b
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-04-28 15:20:00 +0200

    specify devise routes for user and seller models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :sellers
+  devise_for :sellers, path: 'sellers'
   get 'welcome/index'
-  devise_for :users
+  devise_for :users, path: 'users'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
