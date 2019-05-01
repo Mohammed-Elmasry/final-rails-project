a86c58d2d0ddc62aee8d9533059f151e20f736e1
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-01 19:30:25 +0200

    commit for make db migrate for category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_170256) do
+ActiveRecord::Schema.define(version: 2019_05_01_172551) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -34,6 +34,12 @@ ActiveRecord::Schema.define(version: 2019_05_01_170256) do
   end
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
