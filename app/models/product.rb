3dc2469cfcf3aa46c9f56a3ecbff581eb1012f81
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-01 19:28:09 +0200

    commit for add association of category to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
     has_one_attached :image
     belongs_to :brand
+    belongs_to :category
     validates :title, :description, :price, :in_stock_quantity ,:image , presence: true
     
 end
