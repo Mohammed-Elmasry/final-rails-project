b441d151a9342f7c339f08c1794ac8090b80cf61
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-01 18:40:59 +0200

    commit for add association for product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     has_one_attached :image
+    belongs_to :brand
     validates :title, :description, :price, :in_stock_quantity ,:image , presence: true
     
 end
