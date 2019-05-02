638b8f82c79957788cee2d8effd9165a03dcbc88
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-02 22:01:58 +0200

    commit for add search option for description in search method

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ApplicationRecord
     belongs_to :brand
     belongs_to :category
     validates :title, :description, :price, :in_stock_quantity ,:image , presence: true
-    def self.search(search)
+    def self.search(search_from , search_to)
         if search
-            where(["title LIKE ?","%#{search}%"])
+            where(["title LIKE ? and description LIKE ? ","%#{search_from}%" , "%#{search_to}%"])
         else
         all
           end
