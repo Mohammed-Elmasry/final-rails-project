ebd702d38b6c0eec47189a13d1f0994bd9893618
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-02 21:06:53 +0200

    commit for changes in index action remove sql in it to the product's model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.where(["title LIKE ?","%#{params[:search]}%"])
+    @products = Product.search(params[:search])
   end
 
   # GET /products/1
