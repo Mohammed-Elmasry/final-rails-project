073149fdab0f95686bd05c53c0d419e5eb6f6617
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-02 23:16:49 +0200

    rectify seller_objects controller

---
+++ app/controllers/seller_objects_controller.rb
@@ -0,0 +1,25 @@
+class SellerObjectsController < ApplicationController
+    def new
+        # @seller = Seller.new()
+    end
+
+    def create
+        # @seller = Seller.new()
+        @seller = Seller.new(params.require(:seller).permit(:email, :password))
+        if @seller.save
+            render 'new'
+            flash[:success] = "Seller created Successfully"
+        else
+            render 'new'
+            flash[:error] = "Couldn't create Seller, try again!"
+        end
+    end
+
+    def index
+        @sellers = Seller.all
+    end
+
+    def show
+        @seller = Seller.find(params[:id])
+    end
+end
