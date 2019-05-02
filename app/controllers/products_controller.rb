501a6ed855d964105409941c7c84e0b72e363234
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-02 22:17:56 +0200

    commit for rename params in index related to search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.search(params[:search_from] and params[:search_to])
+    @products = Product.search(params[:search])
   end
 
   # GET /products/1
