8333526f432bfb7eab57032e12535a211186aa9b
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-02 14:33:56 +0200

    define bodies of new and create methods in SellerEntity controller

--- app/controllers/seller_entity_controller.rb
+++ app/controllers/seller_entity_controller.rb
@@ -1,7 +1,22 @@
 class SellerEntityController < ApplicationController
-  def new
-  end
+  
+  # GET /resource/sign_up
+   def new
+     @seller = Seller.new
+   end
 
+  # POST /resource
+  
   def create
+    if can? :create, Seller
+      @seller = Seller.new(seller_params)
+      if @seller.save
+        redirect_to @seller
+      else
+        render 'new'
+      end      
+    else
+      authorize! :create, Seller
+    end
   end
 end
