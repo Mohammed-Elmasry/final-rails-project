5b9683db441c244dc86d872b081be8a8ef342746
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-02 16:10:59 +0200

    add resources for SellerEntity

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   resources :categories
   resources :brands
   resources :products
-  #resources :seller_entities
+  resources :seller_entities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
