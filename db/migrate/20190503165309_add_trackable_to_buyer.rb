ac8e73f0d5704e8df1fe8e8c21678644119e1b5c
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-03 19:02:25 +0200

    commit for add some changes in change method

--- db/migrate/20190503165309_add_trackable_to_buyer.rb
+++ db/migrate/20190503165309_add_trackable_to_buyer.rb
@@ -1,6 +1,6 @@
 class AddTrackableToBuyer < ActiveRecord::Migration[5.2]
   def change
-    change_table :buyers do |t|
+    change_table(:buyers)do |t|
       t.integer :sign_in_count
       t.datetime :current_sign_in_at
       t.datetime :last_sign_in_at 
