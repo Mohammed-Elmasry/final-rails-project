a1df203756300218e4bfe2358667eb2ba94faed0
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-03 18:01:53 +0200

    add 'add to cart' link to the products index view for buyers only and define the signature of the function in the products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -65,6 +65,10 @@ class ProductsController < ApplicationController
     end
   end
 
+  def add_to_cart
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_product
