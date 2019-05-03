bce2dc990d55a00c3542bf0d2a9dea17d06d5c42
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-03 18:23:41 +0200

    commit for make db migrate after create migration AddConfirmableToDevice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_02_213909) do
+ActiveRecord::Schema.define(version: 2019_05_03_161258) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,11 @@ ActiveRecord::Schema.define(version: 2019_05_02_213909) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_buyers_on_confirmation_token", unique: true
     t.index ["email"], name: "index_buyers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_buyers_on_reset_password_token", unique: true
   end
