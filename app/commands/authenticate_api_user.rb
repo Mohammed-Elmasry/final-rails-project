4def4b9cd3019d74dc83a2aa5f9fc9837cb00259
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-04 16:22:45 +0200

    commit for rename the instance of useing model to apiuser

--- app/commands/authenticate_api_user.rb
+++ app/commands/authenticate_api_user.rb
@@ -1,20 +1,20 @@
-class AuthenticateApiUser
+class AuthenticateApiuser
     prepend SimpleCommand
     def initialize(email, password)
         @email = email
         @password = password
     end
     def call
-        JsonWebToken.encode(api_user_id: api_user.id) if api_user
+        JsonWebToken.encode(apiuser_id: apiuser.id) if apiuser
      end
      private
-    api_user
+    apiuser
         attr_accessor :email, :password
-        def api_user
-            api_user = ApiUser.find_by_email(email)
-            return api_user if api_user && api_user.authenticate(password)
+        def apiuser
+            apiuser = Apiuser.find_by_email(email)
+            return apiuser if apiuser && apiuser.authenticate(password)
         
-            errors.add :api_user_authentication, 'invalid credentials'
+            errors.add :apiuser_authentication, 'invalid credentials'
             nil
         end
 end
