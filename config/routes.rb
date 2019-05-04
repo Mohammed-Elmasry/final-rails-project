b708d02769100c653bea8265a5ecc44afc504df9
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-04 19:44:01 +0200

    change root route to products#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,34 @@
 Rails.application.routes.draw do
-
   resources :orders
   resources :stores
-  devise_for :buyers, path: 'buyers', controllers: {
-     registerations: 'buyers/registrations',
-     sessions: 'buyers/sessions',
-     confirmation: 'buyers/confirmations',
-     passwords: 'buyers/passwords',
-     registerations: 'buyers/registrations',
-  }
+  devise_for :buyers, path: "buyers", controllers: {
+             registerations: "buyers/registrations",
+             sessions: "buyers/sessions",
+             confirmation: "buyers/confirmations",
+             passwords: "buyers/passwords",
+             registerations: "buyers/registrations",
+           }
   devise_for :sellers, controllers: {
-   #   registrations: 'sellers/registrations',
-     sessions: 'sellers/sessions',
-     confirmation: 'sellers/confirmations',
-     passwords: 'sellers/passwords',
-     registerations: 'sellers/registrations',
-  }
+              #   registrations: 'sellers/registrations',
+              sessions: "sellers/sessions",
+              confirmation: "sellers/confirmations",
+              passwords: "sellers/passwords",
+              registerations: "sellers/registrations",
+            }
 
-  get 'welcome/index'
-  devise_for :users, path: 'users', controllers: {
-    registerations: 'users/registrations',
-    sessions: 'users/sessions',
-    confirmation: 'users/confirmations',
-    passwords: 'users/passwords',
-    registerations: 'users/registrations',
- }
+  get "welcome/index"
+  devise_for :users, path: "users", controllers: {
+                       registerations: "users/registrations",
+                       sessions: "users/sessions",
+                       confirmation: "users/confirmations",
+                       passwords: "users/passwords",
+                       registerations: "users/registrations",
+                     }
   resources :categories
   resources :brands
   resources :products
   resources :seller_objects
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root "products#index"
 end
