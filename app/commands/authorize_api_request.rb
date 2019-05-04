4eeb804c57282c7b64f4402644527bd8eacaa3e6
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-04 07:28:36 +0200

    commit for change user to buyer to work on buyer model

--- app/commands/authorize_api_request.rb
+++ app/commands/authorize_api_request.rb
@@ -4,20 +4,20 @@ class AuthorizeApiRequest
         @headers = headers
     end
     def call
-        user
+        buyer
      end
     private
 
         attr_reader :headers
-  def user
-    @user ||= User.find(decoded_auth_token[:user_id]) if decoded_auth_token
-    @user || errors.add(:token, 'Invalid token') && nil
+  def buyer
+    @buyer||= Buyer.find(decoded_auth_token[:buyer_id]) if decoded_auth_token
+    @buyer || errors.add(:token, 'Invalid token') && nil
   end
 
   def decoded_auth_token
     @decoded_auth_token ||= JsonWebToken.decode(http_auth_header)
   end
-  
+
   def http_auth_header
     if headers['Authorization'].present?
       return headers['Authorization'].split(' ').last
