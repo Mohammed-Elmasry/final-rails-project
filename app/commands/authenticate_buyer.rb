ffacbf4cd2cb478bd7b6dc90be639481bfdbc6e0
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-04 10:48:05 +0200

    commit for change buyer to api_user in AuthenticateApiUser

--- app/commands/authenticate_buyer.rb
+++ app/commands/authenticate_buyer.rb
@@ -1,20 +1,20 @@
-class AuthenticateBuyer
+class AuthenticateApiUser
     prepend SimpleCommand
     def initialize(email, password)
         @email = email
         @password = password
     end
     def call
-        JsonWebToken.encode(buyer_id: buyer.id) if buyer
+        JsonWebToken.encode(api_user_id: api_user.id) if api_user
      end
      private
-
+    api_user
         attr_accessor :email, :password
-        def buyer
-            buyer = Buyer.find_by_email(email)
-            return buyer if buyer && buyer.authenticate(password)
+        def api_user
+            api_user = ApiUser.find_by_email(email)
+            return api_user if api_user && api_user.authenticate(password)
         
-            errors.add :buyer_authentication, 'invalid credentials'
+            errors.add :api_user_authentication, 'invalid credentials'
             nil
         end
 end
