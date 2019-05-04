b76eb714f71090f9e33672e14c2a2c6bde9d2e84
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-04 10:53:27 +0200

    commit for changes the model from buyer to api_user

--- app/controllers/application_api_controller.rb
+++ app/controllers/application_api_controller.rb
@@ -1,11 +1,11 @@
 class ApplicationApiController < ActionController::API
     before_action :authenticate_request
-     attr_reader :current_buyer
+     attr_reader :current_api_user
    
      private
    
      def authenticate_request
-       @current_buyer = AuthorizeApiRequest.call(request.headers).result
-       render json: { error: 'Not Authorized' }, status: 401 unless @current_buyer
+       @current_api_user = AuthorizeApiRequest.call(request.headers).result
+       render json: { error: 'Not Authorized' }, status: 401 unless @current_api_user
      end
    end
