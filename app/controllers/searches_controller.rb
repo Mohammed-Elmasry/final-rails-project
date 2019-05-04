d33dafa435beb9658ab293202337eba4d6f2e669
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-04 22:32:43 +0200

    commit for create method new in store controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,8 @@
 class SearchesController < ApplicationController
+    def new
+        @search = Search.new
+        @categories = Category.distinct(:category)
+        @brands = Brand.distinct(:brand)
+        @stores = Store.distinct(:store)
+    end
 end
