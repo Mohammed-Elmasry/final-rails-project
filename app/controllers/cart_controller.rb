5b40798056ad8317dc6fe4155850dac55956fbb7
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-05 19:26:25 +0200

    define index method in cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,10 +1,27 @@
 class CartController < ApplicationController
   def index
+    @carts = Cart.where(buyer_id: current_buyer.id)
+    @arr = Array.new()
+    if @carts.present? # if it's not an empty set
+      @carts.each do |cart|
+        @cartProduct = Hash.new()
+        @product = Product.find(cart.product.id)
+        puts "the product is #{@product.title}"
+        @cartProduct[:product_name] = @product.title
+        @cartProduct[:product_price] = @product.price
+        @cartProduct[:product_image] = @product.image
+        @cartProduct[:quantity] = cart.qty
+        @cartProduct[:product_total] = @product.price * cart.qty
+        @arr.push(@cartProduct)
+      end
+      # byebug
+    else
+      puts "products don't exist"
+    end
   end
 
   def edit #this is used as the add_to_cart method
     @product_id = params[:id]
-    # begin
     if Product.find(@product_id).in_stock_quantity > 0
       @cart = Cart.where(product_id: @product_id, buyer_id: current_buyer.id)
       if @cart.present? #returned collection has record
@@ -18,5 +35,10 @@ class CartController < ApplicationController
       puts "it's out of stock"
     end
     redirect_to products_url
+  end
+
+  private
+
+  def empty
   end
 end
