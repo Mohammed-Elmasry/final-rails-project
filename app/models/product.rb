211facb134512e77f418b977f710d891d7d59172
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-05 11:54:02 +0200

    commit for add association related to stores

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ApplicationRecord
     has_one_attached :image
+    has_and_belongs_to_many :stores
     belongs_to :brand
     belongs_to :category
     validates :title, :description, :price, :in_stock_quantity ,:image , presence: true
