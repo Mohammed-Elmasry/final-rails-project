c77159cd2fb2f0f703c41c1d7afd01ab84ee8543
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-05 10:37:17 +0200

    commit for make db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_201040) do
+ActiveRecord::Schema.define(version: 2019_05_05_083601) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -93,9 +93,9 @@ ActiveRecord::Schema.define(version: 2019_05_04_201040) do
     t.text "description"
     t.integer "min_price"
     t.integer "max_price"
-    t.string "brand"
-    t.string "category"
-    t.string "store"
+    t.integer "brand_id"
+    t.integer "category_id"
+    t.integer "store_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
