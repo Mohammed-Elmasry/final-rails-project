002c9d6ad330f7244db037b5797e219bb83ad01f
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-05 11:02:01 +0200

    commit for write action create

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,4 +2,9 @@ class SearchesController < ApplicationController
     def new 
         @search = Search.new
     end
+
+    def create
+        @search = Search.create!(params[:search])
+        redirect_to @search
+    end
 end
