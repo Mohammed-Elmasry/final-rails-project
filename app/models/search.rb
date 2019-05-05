9f4cf77ac8b466a7c5356af9d9eb17ea7d5a3b58
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-05 08:43:32 +0200

    commit for write search_products method

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,2 +1,14 @@
 class Search < ApplicationRecord
+    def search_products
+        products = Product.all
+        products = products.where(["title LIKE ?" , "%#{title}%"]) if title.present?
+        products = products.where(["category LIKE ?" , "%#{category}%"]) if category.present?
+        products = products.where(["store LIKE ? " , "%#{store}%"]) if store.present?
+        products = products.where(["description LIKE ? " , "%#{description}%"]) if description.present?
+        products = products.where(["price >= ?" , min_price]) if min_price.present?
+        products = products.where(["price <= ?" , max_price]) if max_price.present?
+        products = products.where(["brand LIKE ?" , "%#{brand}%"]) if brand.present?
+
+        return products
+    end 
 end
