e8cd89ae5458c6648455c18703265558e8abeffc
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-05 22:30:56 +0200

    commit for make db migrate after create OrderStatus model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_05_123438) do
+ActiveRecord::Schema.define(version: 2019_05_05_202728) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +80,12 @@ ActiveRecord::Schema.define(version: 2019_05_05_123438) do
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "order_statuses", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
