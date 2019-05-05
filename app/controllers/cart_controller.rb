de84cc8bcb2e46caed3a0d4ada5388cd8b85d79a
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-05 13:10:02 +0200

    implement add to cart method within cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -2,12 +2,19 @@ class CartController < ApplicationController
   def edit
     @product_id = params[:id]
     begin
-      @cart = Cart.where(buyer_id: current_buyer.id, product_id: @product_id)
-    rescue Exception
-      puts ("order isn't there")
-      @cart = Cart.new(buyer_id: current_buyer.id, product_id: @product_id, qty: 1)
-      @cart.save
-      byebug
+      @cart = Cart.where(product_id: @product_id, buyer_id: current_buyer.id)
+      if @cart.present? #returned collection has record
+        puts("I have the record #{@cart[0].product_id}")
+        puts "the quantity is: #{@cart[0].qty}"
+        @cart[0].update(qty: @cart[0].qty + 1)
+      else #returned collection has no records
+        puts "empty array returned"
+        @cart = Cart.create(buyer_id: current_buyer.id, product_id: @product_id, qty: 1)
+      end
+      # byebug
+    rescue #catch exception in case where method returned an empty set
+      puts "product is never added before"
+      @cart = Cart.create(buyer_id: current_buyer.id, product_id: @product_id, qty: 1)
     end
     redirect_to products_url
   end
