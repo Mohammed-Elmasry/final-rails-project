4212335967e863a6208bbe7927e4d765f55a4786
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-05 13:14:28 +0200

    refactor the edit method within the cart controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,19 +1,13 @@
 class CartController < ApplicationController
   def edit #this is used as the add_to_cart method
     @product_id = params[:id]
-    begin
-      @cart = Cart.where(product_id: @product_id, buyer_id: current_buyer.id)
-      if @cart.present? #returned collection has record
-        puts("I have the record #{@cart[0].product_id}")
-        puts "the quantity is: #{@cart[0].qty}"
-        @cart[0].update(qty: @cart[0].qty + 1)
-      else #returned collection has no records
-        puts "empty array returned"
-        @cart = Cart.create(buyer_id: current_buyer.id, product_id: @product_id, qty: 1)
-      end
-      # byebug
-    rescue #catch exception in case where method returned an empty set
-      puts "product is never added before"
+    # begin
+    @cart = Cart.where(product_id: @product_id, buyer_id: current_buyer.id)
+    if @cart.present? #returned collection has record
+      @cart[0].update(qty: @cart[0].qty + 1)
+      puts "updated the quantity of this product in your cart"
+    else #returned collection has no records
+      puts "empty array returned"
       @cart = Cart.create(buyer_id: current_buyer.id, product_id: @product_id, qty: 1)
     end
     redirect_to products_url
