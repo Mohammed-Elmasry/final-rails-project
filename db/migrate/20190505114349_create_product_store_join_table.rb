f8fc7860941afa15035af88fd3a0787fe1a19472
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-05 13:52:06 +0200

    commit for create the content of migration to make a join table

--- db/migrate/20190505114349_create_product_store_join_table.rb
+++ db/migrate/20190505114349_create_product_store_join_table.rb
@@ -1,4 +1,9 @@
 class CreateProductStoreJoinTable < ActiveRecord::Migration[5.2]
   def change
+    create_join_table :products , :stores
+    create_join_table :products , :stores do |t|
+      t.index :product_id
+      t.index :store_id
+    end
   end
 end
