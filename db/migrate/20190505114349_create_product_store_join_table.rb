94fe235e5af26fdf023210faee2635c411ad14b0
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-05 13:56:01 +0200

    commit for remove the migration becouse it is already exist by rails magic

--- db/migrate/20190505114349_create_product_store_join_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateProductStoreJoinTable < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :products , :stores
-    create_join_table :products , :stores do |t|
-      t.index :product_id
-      t.index :store_id
-    end
-  end
-end
