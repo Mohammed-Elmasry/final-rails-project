d2220fb050298deddc305e6e8a95a002263f3cea
SalmaMeniawy <salmamostafameniawy@gmail.com>
2019-05-06 12:16:50 +0200

    add resources for order

--- config/routes.rb
+++ config/routes.rb
@@ -36,6 +36,7 @@ Rails.application.routes.draw do
   post 'authenticate', to: 'authentication#authenticate'
 
   resources :cart
+  resources :order, only:[:index , :edit ]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "products#index"
 end
