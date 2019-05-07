39f53b1857fdf9370ca29118f49fdc7526e3cbdc
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-05-07 23:51:53 +0200

    clean the code for readability

--- app/controllers/buyers/registrations_controller.rb
+++ app/controllers/buyers/registrations_controller.rb
@@ -62,8 +62,4 @@ class Buyers::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-
-  def show_products
-    puts "my products are #{current_buyer.products}"
-  end
 end
